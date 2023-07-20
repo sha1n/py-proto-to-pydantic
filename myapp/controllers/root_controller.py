74e33a9f55f1b2f64e98f357c1498caa4ec93a09
sha1n <sha1n@users.noreply.github.com>
2023-07-20 20:56:40 +0300

    dep injection example

--- myapp/controllers/root_controller.py
+++ myapp/controllers/root_controller.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from myapp.models.message import Message
 
 router = APIRouter()
 
 
+def get_message() -> str:
+    return "Hello World"
+
+
 @router.get("/")
-async def root() -> Message:
-    return Message(message="Hello World")
+async def root(msg=Depends(get_message)) -> Message:
+    return Message(message=msg)
