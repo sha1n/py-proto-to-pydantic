51e124be0d56b321e217e2e2191b597367e0dceb
sha1n <sha1n@users.noreply.github.com>
2023-07-20 20:56:40 +0300

    dep injection example

--- tests/test_root_controller.py
+++ tests/test_root_controller.py
@@ -3,10 +3,11 @@ import pytest
 from myapp import server
 
 
-@pytest.mark.it("Hello World")
-def test_http_get():
-    client = TestClient(server.app)
-    response = client.get("/")
+@pytest.mark.it
+class TestRootController:
+    def test_http_get(self):
+        client = TestClient(server.app)
+        response = client.get("/")
 
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World"}
